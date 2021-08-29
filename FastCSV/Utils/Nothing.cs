4e211f1f6e33306e3babe8499bc0b27c4ec5ca89
Freddy L <neo-ciber_94@hotmail.com>
2021-08-28 23:45:29 -0400

    Added 'Nothing' type

---
+++ FastCSV/Utils/Nothing.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FastCSV.Utils
+{
+    /// <summary>
+    /// Represents the adsence of a value.
+    /// </summary>
+    public record Nothing
+    {
+        /// <summary>
+        /// Gets the empty instance.
+        /// </summary>
+        public static Nothing Value { get; } = new Nothing();
+
+        private Nothing() { }
+    }
+}
