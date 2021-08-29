99ade8b7d1fc1c32272ab4c1cdd3b0db7c90f53a
Freddy L <neo-ciber_94@hotmail.com>
2021-08-29 01:04:32 -0400

    Added CsvIgnore attribute to ignore fields

---
+++ FastCSV/CsvIgnoreAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FastCSV
+{
+    /// <summary>
+    /// Marks a field or property to be ignored by a csv converter.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class CsvIgnoreAttribute : Attribute {}
+}
