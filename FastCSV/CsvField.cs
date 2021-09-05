6d1b2b12324374a9a8c750ba4925dfa7a3c63173
Freddy L <neo-ciber_94@hotmail.com>
2021-09-05 18:15:17 -0400

    Added docs

--- FastCSV/CsvField.cs
+++ FastCSV/CsvField.cs
@@ -6,16 +6,34 @@ namespace FastCSV
 {
     internal record CsvField
     {
+        /// <summary>
+        /// Original name of the field.
+        /// </summary>
         public string OriginalName { get; }
 
+        /// <summary>
+        /// Name of the field.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Value of the field.
+        /// </summary>
         public object? Value { get; }
 
+        /// <summary>
+        /// Type of the field.
+        /// </summary>
         public Type Type { get; }
 
+        /// <summary>
+        /// Source of the value with is either a field or an property.
+        /// </summary>
         public Either<FieldInfo, PropertyInfo> Source { get; }
 
+        /// <summary>
+        /// Whether the field should be ignored or not.
+        /// </summary>
         public bool Ignore { get; set; }
 
         public CsvField(string originalName, string name, object? value, Type type, Either<FieldInfo, PropertyInfo> source, bool ignore)
