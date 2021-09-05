131230b2db435418ad4cef518b18692f8fd87d9b
Freddy L <neo-ciber_94@hotmail.com>
2021-09-05 18:15:34 -0400

    Added fold

--- FastCSV/Utils/Either.cs
+++ FastCSV/Utils/Either.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace FastCSV.Utils
 {
+    /// <summary>
+    /// Represents with one of two states.
+    /// </summary>
+    /// <typeparam name="TLeft">The type of the value on the left.</typeparam>
+    /// <typeparam name="TRight">The type of the value on the right.</typeparam>
     public readonly struct Either<TLeft, TRight> : IEquatable<Either<TLeft, TRight>>
     {
         private readonly object _value;
@@ -14,13 +19,44 @@ namespace FastCSV.Utils
             _isRight = isRight;
         }
 
+        /// <summary>
+        /// The value is stored in the right.
+        /// </summary>
         public bool IsRight => _isRight;
 
+        /// <summary>
+        /// The value is stored in the left.
+        /// </summary>
         public bool IsLeft => !IsRight;
 
+        /// <summary>
+        /// Gets the value in the left.
+        /// </summary>
         public TLeft Left => IsLeft ? (TLeft)_value : throw new InvalidOperationException("value is right");
 
+        /// <summary>
+        /// Gets the value in the right.
+        /// </summary>
         public TRight Right => IsRight ? (TRight)_value : throw new InvalidOperationException("value is left");
+
+        /// <summary>
+        /// Maps either the right and left value of this instance.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the result.</typeparam>
+        /// <param name="foldLeft">Function to map the left value.</param>
+        /// <param name="foldRight">Function to map the right value.</param>
+        /// <returns>Resulting value.</returns>
+        public TResult Fold<TResult>(Func<TLeft, TResult> foldLeft, Func<TRight, TResult> foldRight)
+        {
+            if (IsRight)
+            {
+                return foldRight(Right);
+            }
+            else
+            {
+                return foldLeft(Left);
+            }
+        }
 
         public override bool Equals(object? obj)
         {
@@ -66,21 +102,45 @@ namespace FastCSV.Utils
 
     public static partial class Either
     {
+        /// <summary>
+        /// Creates an <see cref="Either{TLeft, TRight}"/> from the left value.
+        /// </summary>
+        /// <typeparam name="TLeft">Type of the left value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>An either with a left value.</returns>
         public static Either<TLeft, Nothing> FromLeft<TLeft>(TLeft value)
         {
             return FromLeft<TLeft, Nothing>(value);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Either{TLeft, TRight}"/> from the left value.
+        /// </summary>
+        /// <typeparam name="TLeft">Type of the left value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>An either with a left value.</returns>
         public static Either<TLeft, TRight> FromLeft<TLeft, TRight>(TLeft value)
         {
             return new Either<TLeft, TRight>(value!, isRight: false);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Either{TLeft, TRight}"/> from the right value.
+        /// </summary>
+        /// <typeparam name="TRight">Type of the right value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>An either with a right value.</returns>
         public static Either<Nothing, TRight> FromRight<TRight>(TRight value)
         {
             return FromRight<Nothing, TRight>(value);
         }
 
+        /// <summary>
+        /// Creates an <see cref="Either{TLeft, TRight}"/> from the right value.
+        /// </summary>
+        /// <typeparam name="TRight">Type of the right value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>An either with a right value.</returns>
         public static Either<TLeft, TRight> FromRight<TLeft, TRight>(TRight value)
         {
             return new Either<TLeft, TRight>(value!, isRight: true);
