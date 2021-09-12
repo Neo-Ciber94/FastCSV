22c1da5c48362d77069056d48d416c011a8b9fa0
Freddy L <neo-ciber_94@hotmail.com>
2021-09-11 20:00:08 -0400

    Added tests for nullables without header

--- FastCSVTests/CsvConverterNullableTests.cs
+++ FastCSVTests/CsvConverterNullableTests.cs
@@ -37,5 +37,28 @@ namespace FastCSV.Tests
             Nullable<int> value = CsvConverter.Deserialize<Nullable<int>>("value\n");
             Assert.AreEqual(new Nullable<int>(), value);
         }
+
+        ///////////// No header
+
+        [Test]
+        public void SerializeNullableNoHeaderTest()
+        {
+            var csv = CsvConverter.Serialize(new Nullable<int>(10), typeof(Nullable<int>), new CsvConverterOptions { IncludeHeader = false });
+            Assert.AreEqual("10", csv);
+        }
+
+        [Test]
+        public void SerializeNullNoHeaderTest()
+        {
+            var csv = CsvConverter.Serialize(null, typeof(Nullable<int>), new CsvConverterOptions { IncludeHeader = false });
+            Assert.AreEqual("", csv);
+        }
+
+        [Test]
+        public void DeserializeNullableNoHeaderTest()
+        {
+            Nullable<int> value = CsvConverter.Deserialize<Nullable<int>>("22", new CsvConverterOptions { IncludeHeader = false });
+            Assert.AreEqual(new Nullable<int>(22), value);
+        }
     }
 }
