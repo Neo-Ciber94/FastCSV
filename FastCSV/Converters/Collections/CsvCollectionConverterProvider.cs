fb2e62a641f7c65f624b170e0418d9c32dec75bb
Freddy L <neo-ciber_94@hotmail.com>
2021-10-05 21:56:38 -0400

    Removed unused imports

--- FastCSV/Converters/Collections/CsvCollectionConverterProvider.cs
+++ FastCSV/Converters/Collections/CsvCollectionConverterProvider.cs
@@ -1,12 +1,9 @@
-﻿using FastCSV.Collections;
-using FastCSV.Utils;
+﻿using FastCSV.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FastCSV.Converters.Collections
 {
