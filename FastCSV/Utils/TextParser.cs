78650aea3bbef6c938eb5750f1cb2769769cefc6
Freddy L <neo-ciber_94@hotmail.com>
2021-10-22 21:03:55 -0400

    Added TextParser

---
+++ FastCSV/Utils/TextParser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastCSV.Utils
+{
+    /// <summary>
+    /// An enumerator over a text.
+    /// </summary>
+    public struct TextParser
+    {
+        private readonly ReadOnlyMemory<char> text;
+        private int pos;
+
+        public TextParser(string text) : this(text.AsMemory()) { }
+
+        public TextParser(ReadOnlyMemory<char> text)
+        {
+            this.text = text;
+            pos = -1;
+        }
+
+        public char Current => text.Span[pos];
+
+        public bool CanConsume(ReadOnlySpan<char> other)
+        {
+            if (other.IsEmpty)
+            {
+                return false;
+            }
+
+            ReadOnlySpan<char> rest = Rest.Span;
+
+            if (other.Length > rest.Length)
+            {
+                return false;
+            }
+
+            return rest.StartsWith(other);
+        }
+
+        public int Consume(ReadOnlySpan<char> other)
+        {
+            if (other.IsEmpty)
+            {
+                return 0;
+            }
+
+            ReadOnlySpan<char> rest = Rest.Span;
+
+            if (other.Length > rest.Length)
+            {
+                return 0;
+            }
+
+            if (rest.StartsWith(other))
+            {
+                pos += other.Length;
+                return other.Length;
+            }
+
+            return 0;
+        }
+
+        public ReadOnlyMemory<char> Rest
+        {
+            get
+            {
+                if (pos == -1)
+                {
+                    throw new InvalidOperationException("TextParser is not initialized, must call MoveNext() first");
+                }
+
+                if (pos >= text.Length)
+                {
+                    return ReadOnlyMemory<char>.Empty;
+                }
+
+                return text[pos..];
+            }
+        }
+
+        public Optional<char> Peek
+        {
+            get
+            {
+                int length = text.Length;
+                int next = pos + 1;
+
+                if (next < length)
+                {
+                    return text.Span[next];
+                }
+
+                return default;
+            }
+        }
+
+        public Optional<char> Next()
+        {
+            if (MoveNext())
+            {
+                return Current;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            int next = pos + 1;
+
+            if (next <= text.Length)
+            {
+                pos = next;
+            }
+
+            return next < text.Length;
+        }
+
+        public bool HasNext()
+        {
+            int next = pos + 1;
+            return next < text.Length;
+        }
+
+        public TextParser GetEnumerator() => this;
+    }
+}
---
+++ FastCSVTests/Utils/TextParserTests.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FastCSV.Utils
+{
+    [TestFixture]
+    public class TextParserTests
+    {
+        [Test]
+        public void EnumeratorTest()
+        {
+            var parser = new TextParser("Green");
+
+            Assert.True(parser.MoveNext());
+            Assert.AreEqual('G', parser.Current);
+
+            Assert.True(parser.MoveNext());
+            Assert.AreEqual('r', parser.Current);
+
+            Assert.True(parser.MoveNext());
+            Assert.AreEqual('e', parser.Current);
+
+            Assert.True(parser.MoveNext());
+            Assert.AreEqual('e', parser.Current);
+
+            Assert.True(parser.MoveNext());
+            Assert.AreEqual('n', parser.Current);
+
+            Assert.False(parser.MoveNext());
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            var parser = new TextParser("Green");
+
+            Assert.True(parser.HasNext());
+            Assert.AreEqual('G', parser.Next().Value);
+            Assert.AreEqual('r', parser.Next().Value);
+            Assert.AreEqual('e', parser.Next().Value);
+            Assert.AreEqual('e', parser.Next().Value);
+            Assert.AreEqual('n', parser.Next().Value);
+            Assert.False(parser.MoveNext());
+        }
+
+        [Test]
+        public void PeekTest()
+        {
+            var parser = new TextParser("Anima");
+
+            Assert.AreEqual('A', parser.Peek.Value);
+            parser.MoveNext();
+
+            Assert.AreEqual('n', parser.Peek.Value);
+            parser.MoveNext();
+
+            Assert.AreEqual('i', parser.Peek.Value);
+            parser.MoveNext();
+
+            Assert.AreEqual('m', parser.Peek.Value);
+            parser.MoveNext();
+
+            Assert.AreEqual('a', parser.Peek.Value);
+            parser.MoveNext();
+
+            Assert.False(parser.Peek.HasValue);
+        }
+
+        [Test]
+        public void RestTest()
+        {
+            var parser = new TextParser("Hello World");
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var _ = parser.Rest.ToString();
+            });
+
+            parser.MoveNext();
+            Assert.AreEqual("Hello World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("ello World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("llo World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("lo World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("o World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual(" World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("World", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("orld", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("rld", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("ld", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("d", parser.Rest.ToString());
+
+            parser.MoveNext();
+            Assert.AreEqual("", parser.Rest.ToString());
+
+            Assert.False(parser.MoveNext());
+            Assert.AreEqual(0, parser.Rest.Length);
+        }
+
+        [Test]
+        public void CanConsumeTest()
+        {
+            var parser = new TextParser("Hello World");
+            parser.MoveNext();
+
+            Assert.True(parser.CanConsume("Hello"));
+            Assert.True(parser.CanConsume("Hello World"));
+
+            parser.MoveNext();
+            Assert.False(parser.CanConsume("Hello"));
+            Assert.False(parser.CanConsume("Hello World"));
+
+            Assert.True(parser.CanConsume("ello"));
+            Assert.True(parser.CanConsume("ello World"));
+
+            Assert.False(parser.CanConsume(""));
+
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+
+            Assert.False(parser.CanConsume("d"));
+            Assert.False(parser.CanConsume(""));
+        }
+
+        [Test]
+        public void ConsumeTest()
+        {
+            var parser = new TextParser("Hello World");
+            parser.MoveNext();
+
+            Assert.AreEqual(1, parser.Consume("H"));
+            Assert.AreEqual("ello World", parser.Rest.ToString());
+
+            Assert.AreEqual(5, parser.Consume("ello "));
+            Assert.AreEqual("World", parser.Rest.ToString());
+
+            Assert.AreEqual(5, parser.Consume("World"));
+            Assert.AreEqual("", parser.Rest.ToString());
+        }
+    }
+}
