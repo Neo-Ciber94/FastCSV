484c649ced6bf3f87c1b5c2585f7340016c4d008
Freddy L <neo-ciber_94@hotmail.com>
2021-10-31 03:33:28 -0400

    Added interface for csv property info

---
+++ FastCSV/CsvPropertyData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using FastCSV.Converters;
+
+namespace FastCSV
+{
+    /// <summary>
+    /// Holds information about a csv property.
+    /// </summary>
+    public record CsvPropertyData(CsvPropertyInfo Info, string Name, object? Value) : ICsvPropertyInfo
+    {
+        /// <summary>
+        /// Children of this csv property data.
+        /// </summary>
+        public IReadOnlyList<CsvPropertyData> Children { get; set; } = new List<CsvPropertyData>();
+
+        /// <inheritdoc/>
+        public string OriginalName => Info.OriginalName;
+
+        /// <inheritdoc/>
+        public Type Type => Info.Type;
+
+        /// <inheritdoc/>
+        public MemberInfo Member => Info.Member;
+
+        /// <inheritdoc/>
+        public bool Ignore => Info.Ignore;
+
+        /// <inheritdoc/>
+        public ICsvValueConverter? Converter => Info.Converter;
+
+        /// <inheritdoc/>
+        public bool IsProperty => Info.IsProperty;
+
+        /// <inheritdoc/>
+        public bool IsField => Info.IsField;
+
+        /// <inheritdoc/>
+        public bool IsReadOnly => Info.IsReadOnly;
+
+        /// <inheritdoc/>
+        public object? GetValue(object target) => Info.GetValue(target);
+
+        /// <inheritdoc/>
+        public void SetValue(object target, object? value) => Info.SetValue(target, value);
+    }
+}
---
+++ FastCSV/ICsvPropertyInfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using FastCSV.Converters;
+
+namespace FastCSV
+{
+    /// <summary>
+    /// Provides information about how serialize/deserialize a csv type property.
+    /// </summary>
+    public interface ICsvPropertyInfo
+    {
+        /// <summary>
+        /// Original name of the field.
+        /// </summary>
+        string OriginalName { get; }
+
+        /// <summary>
+        /// Type of the field.
+        /// </summary>
+        Type Type { get; }
+
+        /// <summary>
+        /// Source of the value with is either a field or an property.
+        /// </summary>
+        MemberInfo Member { get; }
+
+        /// <summary>
+        /// Whether the field should be ignored or not.
+        /// </summary>
+        bool Ignore { get; }
+
+        /// <summary>
+        /// The converter for this property.
+        /// </summary>
+        ICsvValueConverter? Converter { get; }
+
+        /// <summary>
+        /// Whether this instance is a property.
+        /// </summary>
+        public bool IsProperty { get; }
+
+        /// <summary>
+        /// Whether this instance is a field.
+        /// </summary>
+        public bool IsField { get; }
+
+        /// <summary>
+        /// Whether this instance is read-only.
+        /// </summary>
+        public bool IsReadOnly { get; }
+
+        /// <summary>
+        /// Sets a value to the member of the target.
+        /// </summary>
+        /// <param name="target">The instance to set the value.</param>
+        /// <param name="value">The value to set.</param>
+        public void SetValue(object target, object? value);
+
+        /// <summary>
+        /// Gets the value of the member of the target.
+        /// </summary>
+        /// <param name="target">The instance to get the value.</param>
+        /// <returns>Get value of the target.</returns>
+        public object? GetValue(object target);
+    }
+}
