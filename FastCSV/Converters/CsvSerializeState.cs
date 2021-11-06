7d1a427217137be1e9920ba363cf31ba262c39aa
Freddy L <neo-ciber_94@hotmail.com>
2021-11-06 19:13:30 -0400

    Updated CsvSerializeState to use IList<string> as buffer

--- FastCSV/Converters/CsvSerializeState.cs
+++ FastCSV/Converters/CsvSerializeState.cs
@@ -9,7 +9,7 @@ namespace FastCSV.Converters
     /// </summary>
     public struct CsvSerializeState
     {
-        private readonly List<string> _buffer;
+        private readonly IList<string> _buffer;
         private readonly object? _value;
 
         /// <summary>
@@ -25,19 +25,34 @@ namespace FastCSV.Converters
         /// <summary>
         /// Gets a readonly list of the current serialized values.
         /// </summary>
-        public IReadOnlyList<string> Serialized => _buffer;
+        public IEnumerable<string> Serialized => _buffer;
+
+        /// <summary>
+        /// Number of currently serialized values.
+        /// </summary>
+        public int Count => _buffer.Count;
 
         /// <summary>
         /// The value being serialized.
         /// </summary>
         public object? Value => _value;
 
-        public CsvSerializeState(CsvConverterOptions options, object? value, List<string> buffer, ICsvValueConverter? converter = null)
+        public CsvSerializeState(CsvConverterOptions options, object? value, IList<string> buffer, ICsvValueConverter? converter = null)
         {
             Options = options;
             Converter = converter;
             _value = value;
             _buffer = buffer;
+        }
+
+        /// <summary>
+        /// Gets a serialized value from the given index.
+        /// </summary>
+        /// <param name="index">The index of the value.</param>
+        /// <returns>A serialized value.</returns>
+        public string GetSerializedValue(int index)
+        {
+            return _buffer[index];
         }
 
         /// <summary>
