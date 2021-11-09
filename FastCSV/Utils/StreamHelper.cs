7f29736ec9c2df6930c750292f58788238275371
Freddy L <neo-ciber_94@hotmail.com>
2021-11-09 01:30:59 -0400

    Updated StreamHelper.CreateStreamFromString to allow write on the returned stream

--- FastCSV/Utils/StreamHelper.cs
+++ FastCSV/Utils/StreamHelper.cs
@@ -11,8 +11,9 @@ namespace FastCSV.Utils
         /// Gets a <see cref="MemoryStream"/> containing the specified <see cref="ReadOnlySpan{char}"/> data.
         /// </summary>
         /// <param name="s">The data.</param>
+        /// <param name="writable">Whether the returned <see cref="MemoryStream"/> should be writable.</param>
         /// <returns>A stream containing the specified data.</returns>
-        public static Stream CreateStreamFromString(ReadOnlySpan<char> s)
+        public static Stream CreateStreamFromString(ReadOnlySpan<char> s, bool writable = false)
         {
             if (s.IsEmpty)
             {
@@ -29,7 +30,16 @@ namespace FastCSV.Utils
             // Returns the array to the pool
             ArrayPool<byte>.Shared.Return(arrayFromPool);
 
-            return new MemoryStream(byteArray);
+            if (writable)
+            {
+                var memoryStream = new MemoryStream(capacity: byteArray.Length);
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+                return memoryStream;
+            }
+            else
+            {
+                return new MemoryStream(byteArray);
+            }
         }
     }
 }
